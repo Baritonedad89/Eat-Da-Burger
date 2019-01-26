fb246390dcae4298e2aba9f8f1a4fe257afeddee
Hero's Dad <cody.monta@gmail.com>
2019-01-25 21:04:11 -0600

    preparing for heroku hosting

--- config/connection.js
+++ config/connection.js
@@ -7,6 +7,16 @@ var connection = mysql.createConnection({
   password: "Baritone2018*",
   database: "burgers_db"
 });
+if(process.env.JAWSDB_URL) {
+  connection =mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Baritone2018*',
+    database: 'burgers_db'
+  });
+};
 
 connection.connect(function(err) {
   if (err) {
